e7af5a441f45744b180f6abd5c4dda9b57779678
tangereye <marchr7@gmail.com>
2019-04-13 21:17:20 +0200

    init, everything is working apart from face recognition

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai'
+import Particles from "react-particles-js";
+
+const app = new Clarifai.App({
+    apiKey: 'ffcb55ca585f499d9d1d3cd03a14dc52'
+});
+
+const params = {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        }
+    }
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value})
+    };
+
+    onSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+            .then(
+                function (response) {
+                    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+                },
+                function (err) {
+                    console.log(err)
+                })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Particles
+                    className='particles'
+                    params={params}/>
+                <Navigation/>
+                <Logo/>
+                <Rank/>
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
+                <FaceRecognition imageUrl={this.state.imageUrl}/>
+            </div>
+        );
+    }
 }
 
 export default App;
